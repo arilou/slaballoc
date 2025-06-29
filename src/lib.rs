f34e8a263c8d18daafda5123b18c8d123b5f924e
Jon Doron <jond@wiz.io>
2025-06-29 08:35:27 +0300

    Lib: Allocator API is not required
    An external user of the lib can use Allocator API if they want to, there
    is no dependency here for the Allocator API.
    
    Signed-off-by: Jon Doron <jond@wiz.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![no_std]
-#![feature(allocator_api, maybe_uninit_slice)]
+#![feature(maybe_uninit_slice)]
 
 mod slab;
 pub use slab::{SlabAlloc, SlabAllocError, SlabAllocator};
